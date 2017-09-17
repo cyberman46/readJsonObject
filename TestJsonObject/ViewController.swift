ca83ce187d7558a45564d94f9809f63485fac814
cyberman46 <akekapon27@gmail.com>
2017-09-17 14:12:48 +0700

    read Json

--- TestJsonObject/ViewController.swift
+++ TestJsonObject/ViewController.swift
@@ -13,6 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string:"https://api.fixer.io/latest")
+        let task = URLSession.shared.dataTask(with: url!){(data,request,error) in
+            if error != nil {
+                print("error!!")
+            }else{
+            
+                if let content = data{
+                    do{
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        let base = myJson["base"] as! String
+                        if let rates = myJson["rates"] as? NSDictionary {
+                            
+                            if let currentCy = rates["JPY"]{
+                                print("Currency of Yen : \(currentCy)")
+                            }
+                            print("base : \(String(describing: base))")
+                        }
+                        
+                    }catch{
+                    
+                    }
+                }
+            }
+            
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
