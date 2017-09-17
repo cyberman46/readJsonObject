9210ad1e36e2744ff14e25791fea8a4fc88c13e3
cyberman46 <akekapon27@gmail.com>
2017-09-17 13:37:19 +0700

    Initial Commit

---
+++ TestJsonObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestJsonObject
+//
+//  Created by Akekapon Suebprom on 9/17/2560 BE.
+//  Copyright © 2560 kratostracking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
